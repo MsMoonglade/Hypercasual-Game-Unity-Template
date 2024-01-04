480747cfb1f960afc8b8eeb4f142360bc34a86ac
MsMoonglade <moonglade95@gmail.com>
2024-01-04 17:55:33 +0100

    ReferenceByValue to Generic Type
    (Need to test)

--- Assets/Scripts/Manager/ReferenceByValue.cs
+++ Assets/Scripts/Manager/ReferenceByValue.cs
@@ -5,47 +5,24 @@ using UnityEngine;
 public class ReferenceByValue : SingletonPersistent<ReferenceByValue>
 {
     [Header("Value Variables")]
-    public int maxValue;
     public int valueSegment;
 
     [Space]
     [Header("Project References")]
     public Material[] possibleMats;
-    public Color[] possibleColor;    
+    public Color[] possibleColor;
 
     /// <summary>
     /// Return Material At given Value
     /// </summary>
     /// <param name="i_value"><given value/param>
     /// <returns></returns>
-    public Material ReturnMaterial(int i_value)
+    public T ReturnValue<T>(T[] possibleValue, int i_index)
     {
-        Material o_mat = null;
-
-        int localIndex = i_value > maxValue ? maxValue : i_value;
+        int localIndex = i_index > possibleValue.Length - 1 ? possibleValue.Length - 1 : i_index;
 
         localIndex = localIndex / valueSegment;
 
-        o_mat = possibleMats[localIndex];
-
-        return o_mat;
-    }
-
-    /// <summary>
-    /// Return Color At given Value
-    /// </summary>
-    /// <param name="i_value"><given value/param>
-    /// <returns></returns>
-    public Color ReturnColor(int i_value)
-    {
-        Color o_color = Color.black;
-
-        int localIndex = i_value > maxValue ? maxValue : i_value;
-
-        localIndex = localIndex / valueSegment;
-
-        o_color = possibleColor[localIndex];
-
-        return o_color;
+        return possibleValue[localIndex];
     }
 }
